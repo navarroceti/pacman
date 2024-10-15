8862b9ee247fa41d469ff6bb382151a888110956
navarroceti <129806280+navarroceti@users.noreply.github.com>
2024-10-15 02:03:19 +0000

    clase 1

---
+++ include/TazoDorado.hpp
@@ -0,0 +1,13 @@
+class TazoDorado
+{
+private:
+    bool flotando;
+public:
+    TazoDorado() {
+        this->flotando = false;
+    }
+    ~TazoDorado() {}
+    void Flotar(){
+        this->flotando = true;
+    }
+};
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <TazoDorado.hpp>
+
+int main(){
+
+    std::cout<<"Hola mundo"<<std::endl;
+}
